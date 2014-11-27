d1f385585f7dfc346e57c6b3bec9d93a7a7ef807
Berendea N. Nicolae <nicolae.berendea@info.uaic.ro>
2014-11-27 23:41:18 +0200

    remove compilation errors

--- client.cpp
+++ client.cpp
@@ -1,13 +1,17 @@
 /*
 */
 
+#include <cstdio>
+#include <cerrno>
+#include <unistd.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 int sPort = 1494;
 
 class Connection{
-    long 
 };
 
 int main()
@@ -15,13 +19,13 @@ int main()
     int sd;
     sockaddr_in server;
     
-    if ((sd=socket(AF_INET, SOCK_DGRAM, 0)) == -1){
+    if ( (sd=socket(AF_INET, SOCK_DGRAM, 0)) == -1){
         perror("Eroare la socket()");
     }
     
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = inet_addr("255.255.255.255");
-    server.sin_port = htons(port);
+    server.sin_port = htons(sPort);
     
     sendto(sd, "text", 5, 0, (sockaddr*)&server, sizeof(server));
     close(sd);
--- server.cpp
+++ server.cpp
@@ -1,8 +1,12 @@
 /*
 */
 
+#include <cstdio>
+#include <cerrno>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 #define PORT 1494
 
@@ -19,9 +23,9 @@ enum MSG{
 int main()
 {
     int sd;
-    sockaddr_in server = {};
+    sockaddr_in server={};
     
-    if ( (sd=socket(AF_INET, SOCK_DGRAM, 0) == -1){
+    if ( (sd=socket(AF_INET, SOCK_DGRAM, 0)) == -1){
         perror("Nu a putut fi creat socket-ul");
         return errno;
     }
@@ -29,7 +33,7 @@ int main()
     
     server.sin_family = AF_INET;
     server.sin_port = htons(PORT);
-    server.sin_addr = htonl(INADDR_ANY);
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
     if (bind(sd, (sockaddr*)&server, sizeof(sockaddr))==-1){
         perror("Eroare la bind().\n");
         return errno;
@@ -37,9 +41,12 @@ int main()
 
     bool quit = 0;
     while (!quit){
-        int len, sockSize;
+        socklen_t sockSize;
+        int len;
         char msg[100];
-        if ( (len=recvfrom(sd, msg, 100, 0, (sockaddr *)&client, &sockSize)<=0 ){
+        sockaddr_in client={};
+        
+        if ( (len=recvfrom(sd, msg, 100, 0, (sockaddr *)&client, &sockSize)<=0) ){
             perror("Eroare la primire");
         }
         printf("%s", msg);
